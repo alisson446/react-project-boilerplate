19487a4bd51a9785a5208a108a7166efc8b73d0a
Alisson <alissonoliveira446@gmail.com>
2018-09-20 09:53:47 -0300

    react-router and redux configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import routes from './router'
+import appReducer from './reducers'
+
+const store = createStore(appReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        {
+          routes.map((route) => (
+            <Route {...route} />
+          ))
+        }
+      </Switch>
+    </ BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
